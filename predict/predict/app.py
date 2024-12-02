b78c88455549a1307b6bcc0aa970c253800c0710
nlahary <nathanaellahary@gmail.com>
2024-12-02 09:12:58 +0100

    feat(api): use config package - use python3.10 typing - get endpoint for model info - exceptions handling

--- predict/predict/app.py
+++ predict/predict/app.py
@@ -1,64 +1,120 @@
+import logging
+from typing import Optional
 from preprocessing.preprocessing.embeddings import embed
 from fastapi.responses import RedirectResponse
 from fastapi import FastAPI, HTTPException
 from collections import OrderedDict
-from utils import ARTEFACT_PATH
 from functools import lru_cache
-from dotenv import load_dotenv
 from pydantic import BaseModel
-from typing import List
-
 import numpy as np
-import os
+from pathlib import Path
+from config import settings
 
-load_dotenv()
-VERSION = os.getenv("API_VERSION")
+# load_dotenv(dotenv_path=Path(__file__).resolve().parents[2] / '.dev.env')
+
+# VERSION = os.getenv("API_VERSION")
+# ARTEFACTS_PATH = os.getenv("ARTEFACTS_PATH")
+# API_HOST = "0.0.0.0" if os.getenv("HOSTNAME") else os.getenv("API_HOST")
+# API_PORT = int(os.getenv("API_PORT", 3000))
+
+# logs_dir = Path('/app/logs')
+# log_file = logs_dir / 'api.log'
+# logs_dir.mkdir(parents=True, exist_ok=True)
+
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#     handlers=[
+#         logging.StreamHandler(),  # To display logs in the console
+#         # To save logs in a file
+#         logging.FileHandler(
+#             filename=str(log_file)  # Use the full file path
+#         )
+#     ]
+# )
 
 app = FastAPI()
 
 
 @lru_cache()
 def get_model():
-    import run
-    return run.TextPredictionModel.from_artefacts(
-        ARTEFACT_PATH / "2024-11-27-08-32-43"
-    )
+    """ Load the model once and cache it """
+    try:
+        from run import TextPredictionModel
+        return TextPredictionModel.from_artefacts(
+            Path(settings.ARTEFACTS_PATH) / "2024-12-01-08-54-59"
+        )
+    except FileNotFoundError as e:
+        raise HTTPException(
+            status_code=500, detail=f'Model not found: {e}')
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f'Error loading model: {e}')
 
 
 class PredictionRequest(BaseModel):
-    title: List[str]
+    title: list[str]
 
 
 class PredictionResponse(BaseModel):
-    title: List[str]
-    predicted_tag: List[List[str]]
+    title: list[str]
+    predicted_tag: list[list[str]]
+
+
+class ModelSummary(BaseModel):
+    layers: list[tuple[str, tuple[Optional[int], int]]]
+    trainable_params: int
+    non_trainable_params: int
 
 
 @ app.get("/")
 async def root():
+    logging.info("Redirecting to /docs")
     return RedirectResponse(url="/docs")
 
 
-@ app.post(f"/{VERSION}/predict", response_model=PredictionResponse)
-async def predict_tag(request: PredictionRequest):
-    print('request:', request)
+@ app.get('/model')
+async def get_model_info():
+    logging.info("Getting model summary")
     model = get_model()
-    if isinstance(request.title, str):
-        request.title = [request.title]
-    embeddings = embed(request.title)
-    top_5_predictions: list[OrderedDict] = model.model.predict(embeddings)
-    top_k_tags = []
-    for prediction in top_5_predictions:
-        top_k_tags.append([model.labels_index_inv[ind]
-                          for ind in np.argsort(prediction)[::-1][:5]])
+    return ModelSummary(
+        layers=[(layer.name, layer.output_shape)
+                for layer in model.model.layers],
+        trainable_params=model.model.count_params(),
+        non_trainable_params=sum(
+            [np.prod(layer.output_shape[1:]) for layer in model.model.layers])
+    )
+
+
+@ app.post(f"/{settings.API_VERSION}/predict", response_model=PredictionResponse)
+async def predict_tag(request: PredictionRequest):
+    logging.info(f"Predicting tags for: {request.title}")
+    model = get_model()
+    logging.info("Model loaded successfully")
+    try:
+        if isinstance(request.title, str):
+            request.title = [request.title]
+        logging.info(f"Embedding: {request.title}")
+        embeddings = embed(request.title)
+        logging.info(f"Predicting: {request.title}")
+        top_5_predictions: list[OrderedDict] = model.model.predict(embeddings)
+        top_k_tags = []
+        for prediction in top_5_predictions:
+            top_k_tags.append([model.labels_index_inv[ind]
+                               for ind in np.argsort(prediction)[::-1][:5]])
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f'Error predicting tags: {e}')
+
     return PredictionResponse(
         title=request.title,
         predicted_tag=top_k_tags
     )
 
 if __name__ == "__main__":
+
     import uvicorn
     uvicorn.run("app:app",
-                host=os.getenv("API_HOST", "localhost"),
-                port=int(os.getenv("API_PORT", 3000)),
+                host=settings.API_HOST,
+                port=settings.API_PORT,
                 reload=True)
