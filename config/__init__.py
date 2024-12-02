ca2f699af51a471937113187b829e11f1c26ed7a
nlahary <nathanaellahary@gmail.com>
2024-12-02 23:00:55 +0100

    fix(settings): fix BERT_MODEL_PATH. Make sur it points to the snapshots directory of the downloaded model

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional
+import os
+import logging
 
 
 class Settings(BaseSettings):
@@ -11,22 +13,40 @@ class Settings(BaseSettings):
     ARTEFACTS_PATH: str
     DATASET_PATH: str
     # If no path is provided, download the model from Hugging Face
-    BERT_MODEL_PATH: Optional[str] = 'bert-base-uncased'
+    BERT_MODEL_PATH: Optional[str] = None
 
     HOSTNAME: Optional[str] = None
 
     model_config = SettingsConfigDict(
         env_file="./.dev.env",
         env_file_encoding="utf-8",
-        extra='allow'
     )
 
     def model_post_init(self, __context) -> None:
-        """Post-initialization to adjust API_HOST wether we are running
-        in a container or not. HOSTNAME is an env var only available in a container.
-        """
+
         if self.HOSTNAME is not None:
             self.API_HOST = "0.0.0.0"
+
+        if not self.BERT_MODEL_PATH or not os.path.exists(self.BERT_MODEL_PATH):
+            logging.warning(
+                f"BERT_MODEL_PATH ({self.BERT_MODEL_PATH}) not provided or does not exist . The model will be downloaded from Hugging Face."
+            )
+            self.BERT_MODEL_PATH = 'bert-base-uncased'
+        else:
+            snapshots_path = os.path.join(self.BERT_MODEL_PATH, "snapshots")
+            if os.path.exists(snapshots_path) and os.path.isdir(snapshots_path):
+                snapshots = os.listdir(snapshots_path)
+                if snapshots:
+                    logging.info(
+                        f"Snapshots found in {snapshots_path}. Loading the latest snapshot.")
+                    self.BERT_MODEL_PATH = os.path.join(
+                        snapshots_path, sorted(snapshots)[-1])
+                else:
+                    logging.warning(
+                        f"No snapshots directory found in {snapshots_path}. Please provide a valid path.")
+            else:
+                logging.warning(
+                    f"Snapshots path {snapshots_path} does not exist.")
 
 
 settings = Settings()
--- predict/tests/test_predict.py
+++ predict/tests/test_predict.py
@@ -1,17 +1,15 @@
 import unittest
 
 from predict.predict import run
+from config import settings
 from pathlib import Path
-
-ARTEFACT_PATH = Path(__file__).resolve(
-    strict=True).parent.parent.parent / "train/data/artefacts"
 
 
 class PredictTest(unittest.TestCase):
     def test_from_artefacts(self):
         model_timestamp = "2024-12-01-08-54-59"
         model = run.TextPredictionModel.from_artefacts(
-            ARTEFACT_PATH / model_timestamp)
+            Path(settings.ARTEFACTS_PATH) / model_timestamp)
 
         self.assertIsNotNone(model.model)
         self.assertIsNotNone(model.params)
@@ -20,7 +18,7 @@ class PredictTest(unittest.TestCase):
     def test_predict_equal_length(self):
         model_timestamp = "2024-12-01-08-54-59"
         model = run.TextPredictionModel.from_artefacts(
-            ARTEFACT_PATH / model_timestamp)
+            Path(settings.ARTEFACTS_PATH) / model_timestamp)
 
         text_list = [
             "Is it possible to execute the procedure of a function in the scope of the caller?",
